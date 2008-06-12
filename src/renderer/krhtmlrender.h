9528b76a303b3068921f72ee9e486a3575c8bd41
Adam Pigg <adam@piggz.co.uk>
2008-06-12 20:21:31 +0000

    A test 'shape' object (in development) A html export option Selective compilation of the kspread export
    svn path=/trunk/koffice/; revision=819994

---
+++ src/renderer/krhtmlrender.h
@@ -0,0 +1,63 @@
+/*
+ * OpenRPT report writer and rendering engine
+ * Copyright (C) 2001-2007 by OpenMFG, LLC
+ * Copyright (C) 2007-2008 by Adam Pigg (adam@piggz.co.uk)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Please contact info@openmfg.com with any questions on this license.
+ */
+#ifndef __KRHTMLRENDER_H__
+#define __KRHTMLERENDER_H__
+
+#include <QDomDocument>
+#include <QRectF>
+#include <QString>
+#include <QFont>
+
+class KRHtmlRenderPrivate;
+class ParameterList;
+class ORODocument;
+namespace KexiDB
+{
+	class Connection;
+}
+//
+// KRHtmlRender
+// 
+class KRHtmlRender {
+  public:
+    KRHtmlRender(KexiDB::Connection*c=0);
+    KRHtmlRender(const QString &, KexiDB::Connection*c=0);
+
+    virtual ~KRHtmlRender();
+
+    QString generate();
+
+    void setDatabase(KexiDB::Connection*);
+    KexiDB::Connection* database() const;
+
+    bool setDom(const QString &);
+
+    bool isValid() const;
+
+  protected:
+
+  private:
+    KRHtmlRenderPrivate* _internal;
+    
+};
+
+
+#endif
