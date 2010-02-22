KDE/kreport

commit 329e21ea283c6b79287129ffae6f40b2e7e73ce4

Reports *add constness *replace boolean attribute with enums in KRPos and KRSize *remove warnings *add missing translation to properties
svn path=/trunk/koffice/; revision=1093353

src/wrtembed/reportdesigner.cpp
@@ -49,11 +49,19 @@
 #include <KoDpi.h>
 #include <KoPageFormat.h>
 #include <kaction.h>
-#include <kdebug.h>
+#include <KLocale>
+#include <KDebug>
 #include <kross/core/manager.h>
 
 //! Also add public method for runtime?
 const char* ns = "http://kexi-project.org/report/2.0";
+
+static QDomElement propertyToElement(QDomDocument* d, KoProperty::Property* p)
+{
+    QDomElement e = d->createElement("report:" + p->name().toLower());
+    e.appendChild(d->createTextNode(p->value().toString()));
+    return e;
+}
 
 //
 // define and implement the ReportWriterSectionData class
@@ -168,8 +176,8 @@ void ReportDesigner::init()
     connect(d->pageButton, SIGNAL(released()), this, SLOT(slotPageButton_Pressed()));
     emit pagePropertyChanged(*m_set);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     changeSet(m_set);
 }
@@ -286,14 +294,14 @@ QDomElement ReportDesigner::document() const
     content.appendChild(propertyToElement(&doc, m_title));
 
     QDomElement scr = propertyToElement(&doc, m_script);
-    addPropertyAsAttribute(&scr, m_interpreter);
+    ReportEntity::addPropertyAsAttribute(&scr, m_interpreter);
     content.appendChild(scr);
 
     QDomElement grd = doc.createElement("report:grid");
-    addPropertyAsAttribute(&grd, m_showGrid);
-    addPropertyAsAttribute(&grd, m_gridDivisions);
-    addPropertyAsAttribute(&grd, m_gridSnap);
-    addPropertyAsAttribute(&grd, m_unit);
+    ReportEntity::addPropertyAsAttribute(&grd, m_showGrid);
+    ReportEntity::addPropertyAsAttribute(&grd, m_gridDivisions);
+    ReportEntity::addPropertyAsAttribute(&grd, m_gridSnap);
+    ReportEntity::addPropertyAsAttribute(&grd, m_unit);
     content.appendChild(grd);
 
     // pageOptions
@@ -310,12 +318,12 @@ QDomElement ReportDesigner::document() const
         pagestyle.setAttribute("report:page-label-type", m_labelType->value().toString());
     } else {
         pagestyle.appendChild(doc.createTextNode("predefined"));
-	addPropertyAsAttribute(&pagestyle, m_pageSize);
+	ReportEntity::addPropertyAsAttribute(&pagestyle, m_pageSize);
         //pagestyle.setAttribute("report:page-size", m_pageSize->value().toString());
     }
 
     // -- orientation
-    addPropertyAsAttribute(&pagestyle, m_orientation);
+    ReportEntity::addPropertyAsAttribute(&pagestyle, m_orientation);
 
     // -- margins
     pagestyle.setAttribute("fo:margin-top", KoUnit::unit(m_topMargin->option("unit").toString()).toUserStringValue(m_topMargin->value().toDouble()) + m_topMargin->option("unit").toString());
@@ -592,19 +600,19 @@ void ReportDesigner::createProperties()
     QStringList keys, strings;
     m_set = new KoProperty::Set(0, "Report");
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
-    m_title = new KoProperty::Property("Title", "Report", "Title", "Report Title");
+    m_title = new KoProperty::Property("Title", "Report", i18n("Title"), i18n("Report Title"));
 
     keys.clear();
     keys = pageFormats();
-    m_pageSize = new KoProperty::Property("page-size", keys, keys, "A4", "Page Size");
+    m_pageSize = new KoProperty::Property("page-size", keys, keys, "A4", i18n("Page Size"));
 
     keys.clear(); strings.clear();
     keys << "portrait" << "landscape";
     strings << i18n("Portrait") << i18n("Landscape");
-    m_orientation = new KoProperty::Property("print-orientation", keys, strings, "portrait", "Page Orientation");
+    m_orientation = new KoProperty::Property("print-orientation", keys, strings, "portrait", i18n("Page Orientation"));
 
     keys.clear(); strings.clear();
 
@@ -615,25 +623,29 @@ void ReportDesigner::createProperties()
         keys << unit;
     }
 
-    m_unit = new KoProperty::Property("page-unit", keys, strings, "cm", "Page Unit");
+    m_unit = new KoProperty::Property("page-unit", keys, strings, "cm", i18n("Page Unit"));
 
-    m_showGrid = new KoProperty::Property("grid-visible", true, "Show Grid", "Show Grid");
-    m_gridSnap = new KoProperty::Property("grid-snap", true, "Grid Snap", "Grid Snap");
-    m_gridDivisions = new KoProperty::Property("grid-divisions", 4, "Grid Divisions", "Grid Divisions");
+    m_showGrid = new KoProperty::Property("grid-visible", true, i18n("Show Grid"), i18n("Show Grid"));
+    m_gridSnap = new KoProperty::Property("grid-snap", true, i18n("Grid Snap"), i18n("Grid Snap"));
+    m_gridDivisions = new KoProperty::Property("grid-divisions", 4, i18n("Grid Divisions"), i18n("Grid Divisions"));
 
-    m_leftMargin = new KoProperty::Property("margin-left", KoUnit::unit("cm").fromUserValue(1.0), "Left Margin", "Left Margin", KoProperty::Double);
-    m_rightMargin = new KoProperty::Property("margin-right", KoUnit::unit("cm").fromUserValue(1.0), "Right Margin", "Right Margin", KoProperty::Double);
-    m_topMargin = new KoProperty::Property("margin-top", KoUnit::unit("cm").fromUserValue(1.0), "Top Margin", "Top Margin", KoProperty::Double);
-    m_bottomMargin = new KoProperty::Property("margin-bottom", KoUnit::unit("cm").fromUserValue(1.0), "Bottom Margin", "Bottom Margin", KoProperty::Double);
+    m_leftMargin = new KoProperty::Property("margin-left", KoUnit::unit("cm").fromUserValue(1.0),
+        i18n("Left Margin"), i18n("Left Margin"), KoProperty::Double);
+    m_rightMargin = new KoProperty::Property("margin-right", KoUnit::unit("cm").fromUserValue(1.0),
+        i18n("Right Margin"), i18n("Right Margin"), KoProperty::Double);
+    m_topMargin = new KoProperty::Property("margin-top", KoUnit::unit("cm").fromUserValue(1.0),
+        i18n("Top Margin"), i18n("Top Margin"), KoProperty::Double);
+    m_bottomMargin = new KoProperty::Property("margin-bottom", KoUnit::unit("cm").fromUserValue(1.0),
+        i18n("Bottom Margin"), i18n("Bottom Margin"), KoProperty::Double);
     m_leftMargin->setOption("unit", "cm");
     m_rightMargin->setOption("unit", "cm");
     m_topMargin->setOption("unit", "cm");
     m_bottomMargin->setOption("unit", "cm");
 
     keys = Kross::Manager::self().interpreters();
-    m_interpreter = new KoProperty::Property("script-interpreter", keys, keys, keys[0], "Script Interpreter");
+    m_interpreter = new KoProperty::Property("script-interpreter", keys, keys, keys[0], i18n("Script Interpreter"));
 
-    m_script = new KoProperty::Property("script", keys, keys, "", "Object Script");
+    m_script = new KoProperty::Property("script", keys, keys, QString(), i18n("Object Script"));
 
     m_set->addProperty(m_title);
     m_set->addProperty(m_pageSize);

src/wrtembed/reportentitybarcode.cpp
@@ -42,8 +42,8 @@ void ReportEntityBarcode::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     setMaxLength(5);
     ReportRectEntity::init(&m_pos, &m_size, m_set);

src/wrtembed/reportentitychart.cpp
@@ -40,8 +40,8 @@ void ReportEntityChart::init(QGraphicsScene* scene, ReportDesigner *designer)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
     setZValue(Z);

src/wrtembed/reportentitycheck.cpp
@@ -39,8 +39,8 @@ void ReportEntityCheck::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 
-    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityfield.cpp
@@ -40,8 +40,8 @@ void ReportEntityField::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
     
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityimage.cpp
@@ -45,8 +45,8 @@ void ReportEntityImage::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 

src/wrtembed/reportentitylabel.cpp
@@ -40,8 +40,8 @@ void ReportEntityLabel::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 
-    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityline.cpp
@@ -53,8 +53,8 @@ void ReportEntityLine::init(QGraphicsScene* s, ReportDesigner *r)
     if (s)
         s->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set &, KoProperty::Property &)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set &, KoProperty::Property &)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
 
     setZValue(Z);
 }
@@ -136,13 +136,16 @@ void ReportEntityLine::buildXML(QDomDocument & doc, QDomElement & parent)
 void ReportEntityLine::slotPropertyChanged(KoProperty::Set &s, KoProperty::Property &p)
 {
     Q_UNUSED(s);
-    
+
     if (p.name() == "Start" || p.name() == "End") {
-	if (p.name() == "Start") m_start.setUnitPos(p.value().toPointF(), false);
-	if (p.name() == "End") m_end.setUnitPos(p.value().toPointF(), false);
-	
-        setLine ( m_start.toScene().x(), m_start.toScene().y(), m_end.toScene().x(), m_end.toScene().y() );
-    } else if (p.name() == "Name") {
+        if (p.name() == "Start")
+            m_start.setUnitPos(p.value().toPointF(), KRPos::DontUpdateProperty);
+        if (p.name() == "End")
+            m_end.setUnitPos(p.value().toPointF(), KRPos::DontUpdateProperty);
+
+        setLine(m_start.toScene().x(), m_start.toScene().y(), m_end.toScene().x(), m_end.toScene().y());
+    }
+    else if (p.name() == "Name") {
         //For some reason p.oldValue returns an empty string
         if (!m_reportDesigner->isEntityNameUnique(p.value().toString(), this)) {
             p.setValue(m_oldName);
@@ -150,7 +153,8 @@ void ReportEntityLine::slotPropertyChanged(KoProperty::Set &s, KoProperty::Prope
             m_oldName = p.value().toString();
         }
     }
-    if (m_reportDesigner) m_reportDesigner->setModified(true);
+    if (m_reportDesigner)
+        m_reportDesigner->setModified(true);
 }
 
 void ReportEntityLine::mousePressEvent(QGraphicsSceneMouseEvent * event)
@@ -254,6 +258,6 @@ void ReportEntityLine::setUnit(KoUnit u)
 
 void ReportEntityLine::setLineScene(QLineF l)
 {
-    m_start.setScenePos(l.p1(), false);
+    m_start.setScenePos(l.p1(), KRPos::DontUpdateProperty);
     m_end.setScenePos(l.p2());
 }

commit 00d1650fb7ab590462210a47e40fa2d1c7e93d8b

fixed sig/slot connections badly broken by me in r1093353
svn path=/trunk/koffice/; revision=1094572

src/wrtembed/reportdesigner.cpp
@@ -176,8 +176,8 @@ void ReportDesigner::init()
     connect(d->pageButton, SIGNAL(released()), this, SLOT(slotPageButton_Pressed()));
     emit pagePropertyChanged(*m_set);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     changeSet(m_set);
 }
@@ -600,8 +600,8 @@ void ReportDesigner::createProperties()
     QStringList keys, strings;
     m_set = new KoProperty::Set(0, "Report");
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     m_title = new KoProperty::Property("Title", "Report", i18n("Title"), i18n("Report Title"));
 

src/wrtembed/reportentitybarcode.cpp
@@ -42,8 +42,8 @@ void ReportEntityBarcode::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     setMaxLength(5);
     ReportRectEntity::init(&m_pos, &m_size, m_set);

src/wrtembed/reportentitychart.cpp
@@ -40,8 +40,8 @@ void ReportEntityChart::init(QGraphicsScene* scene, ReportDesigner *designer)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
     setZValue(Z);

src/wrtembed/reportentitycheck.cpp
@@ -39,8 +39,8 @@ void ReportEntityCheck::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 
-    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityfield.cpp
@@ -40,8 +40,8 @@ void ReportEntityField::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
     
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityimage.cpp
@@ -45,8 +45,8 @@ void ReportEntityImage::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 

src/wrtembed/reportentitylabel.cpp
@@ -40,8 +40,8 @@ void ReportEntityLabel::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 
-    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityline.cpp
@@ -53,8 +53,8 @@ void ReportEntityLine::init(QGraphicsScene* s, ReportDesigner *r)
     if (s)
         s->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     setZValue(Z);
 }

src/wrtembed/reportentityshape.cpp
@@ -41,8 +41,8 @@ void ReportEntityShape::init(QGraphicsScene * scene)
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 
-    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     setZValue(Z);
 }

src/wrtembed/reportentitytext.cpp
@@ -42,8 +42,8 @@ void ReportEntityText::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(properties(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
 
     ReportRectEntity::init(&m_pos, &m_size, m_set);
 

src/wrtembed/reportsection.cpp
@@ -80,8 +80,8 @@ ReportSection::ReportSection(ReportDesigner * rptdes, const char * name)
     Q_UNUSED(name)
 
     m_sectionData = new KRSectionData();
-    connect(m_sectionData->properties(), SIGNAL(propertyChanged(KoProperty::Set, KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set, KoProperty::Property)));
+    connect(m_sectionData->properties(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
     int dpiY = KoDpi::dpiY();
 
     m_reportDesigner = rptdes;
@@ -107,9 +107,10 @@ ReportSection::ReportSection(ReportDesigner * rptdes, const char * name)
 
     m_resizeBar = new ReportResizeBar(this);
 
-    QObject::connect(m_resizeBar, SIGNAL(barDragged(int)), this, SLOT(slotResizeBarDragged(int)));
-    QObject::connect(m_reportDesigner, SIGNAL(pagePropertyChanged(KoProperty::Set &)), this, SLOT(slotPageOptionsChanged(KoProperty::Set &)));
-    QObject::connect(m_scene, SIGNAL(clicked()), this, (SLOT(slotSceneClicked())));
+    connect(m_resizeBar, SIGNAL(barDragged(int)), this, SLOT(slotResizeBarDragged(int)));
+    connect(m_reportDesigner, SIGNAL(pagePropertyChanged(KoProperty::Set&)),
+        this, SLOT(slotPageOptionsChanged(KoProperty::Set&)));
+    connect(m_scene, SIGNAL(clicked()), this, (SLOT(slotSceneClicked())));
 
     glayout->addWidget(m_title, 0, 0, 1, 2);
     glayout->addWidget(m_sectionRuler, 1, 0);