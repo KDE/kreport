2999cfe6689315a1d161b2ed6fba3ae7611b1081
Jaroslaw Staniek <staniek@kde.org>
2015-05-11 15:26:43 +0200

    kreport example - now loads a sample report document (doesn't render)

--- examples/window.cpp
+++ examples/window.cpp
@@ -4,7 +4,7 @@
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+   version 2.1 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,28 +14,72 @@
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+   Boston, MA 02110-1301, USA.
 */
 
 #include "window.h"
 
-#include <QDate>
-#include <QDateTime>
-#include <QPixmap>
-#include <QStringList>
-#include <QCursor>
-#include <QApplication>
-#include <QDesktopWidget>
-#include <QVBoxLayout>
-#include <QIcon>
-#include <QCommandLineParser>
+#include <QDomElement>
+#include <QFile>
 #include <QDebug>
 
+#include <KoReportPluginManager>
+
 Window::Window()
-        : QWidget()
+    : QMainWindow()
 {
+    createMenus();
+
+    m_view = new QGraphicsView(this);
+    setCentralWidget(m_view);
+
+    // For now based on KexiReportView
+    m_scene = new QGraphicsScene(this);
+    m_scene->setSceneRect(0, 0, 1000, 2000);
+    m_view->setScene(m_scene);
+
+    if (loadDocument()) {
+        m_preRenderer = new KoReportPreRenderer(m_document.documentElement());
+        if (!m_preRenderer->isValid()) {
+            return;
+        }
+    }
+
+    KoReportPluginManager* manager = KoReportPluginManager::self();
+    // TODO
 }
 
 Window::~Window()
 {
+}
+
+bool Window::loadDocument()
+{
+    qDebug() << KREPORTEXAMPLE_DATA_DIR;
+    QFile file(QLatin1String(KREPORTEXAMPLE_DATA_DIR) + "/report.xml");
+    if (!file.open(QIODevice::ReadOnly)) {
+        return false;
+    }
+    QString errorMsg;
+    int errorLine;
+    int errorColumn;
+    if (!m_document.setContent(&file,
+                               &errorMsg, &errorLine, &errorColumn))
+    {
+        qWarning() << "Error reading XML from" << file.fileName()
+                   << "Message:" << errorMsg
+                   << "Line:" << errorLine
+                   << "Column:" << errorColumn;
+        return false;
+    }
+    return true;
+}
+
+void Window::createMenus()
+{
+    m_fileMenu = menuBar()->addMenu(tr("&File"));
+    m_exitAction = new QAction(tr("E&xit"), this);
+    m_exitAction->setShortcuts(QKeySequence::Quit);
+    connect(m_exitAction, SIGNAL(triggered()), this, SLOT(close()));
+    m_fileMenu->addAction(m_exitAction);
 }
--- examples/window.h
+++ examples/window.h
@@ -4,7 +4,7 @@
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+   version 2.1 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,17 +14,23 @@
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+   Boston, MA 02110-1301, USA.
 */
 
-#ifndef WINDOW_H
-#define WINDOW_H
+#ifndef KREPORTEXAMPLE_WINDOW_H
+#define KREPORTEXAMPLE_WINDOW_H
 
-#include <QWidget>
+#include <QGraphicsView>
+#include <QMainWindow>
+#include <QDomDocument>
+#include <QMenuBar>
+#include <QAction>
+
+#include <KoReportPreRenderer>
 
 /*! @internal
  * @short KreportExample application's main window */
-class Window : public QWidget
+class Window : public QMainWindow
 {
     Q_OBJECT
 public:
@@ -32,6 +38,16 @@ public:
     virtual ~Window();
 
 private:
+    void createMenus();
+    bool loadDocument();
+
+    QMenu *m_fileMenu;
+    QAction *m_exitAction;
+
+    QGraphicsView *m_view;
+    QGraphicsScene *m_scene;
+    KoReportPreRenderer *m_preRenderer;
+    QDomDocument m_document;
 };
 
 #endif
