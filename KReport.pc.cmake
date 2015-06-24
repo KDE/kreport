c59c1055c639bf0551deea69aea576151caccc7a
Jaroslaw Staniek <staniek@kde.org>
2015-06-24 21:27:26 +0200

    Rename kreport.pc to KReport.pc per convention
    +update URL and deps

--- KReport.pc.cmake
+++ KReport.pc.cmake
@@ -6,7 +6,7 @@ includedir=${prefix}/include
 Name: KReport
 Description: A framework for the creation and generation of reports in multiple formats
 Version: @KREPORT_VERSION@
-URL: https://projects.kde.org/kreport
-Requires: Qt5Core Qt5Gui Qt5Widgets Qt5Xml Qt5PrintSupport KF5CoreAddons KF5WidgetsAddons KF5WidgetsAddons KProperty
+URL: https://community.kde.org/KReport
+Requires: Qt5Core Qt5Gui Qt5Widgets Qt5Xml Qt5PrintSupport KF5CoreAddons KF5WidgetsAddons KF5GuiAddons KProperty
 Libs: -L${libdir}
 Cflags: -I${includedir}
