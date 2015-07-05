d69129961c98ca757208b17799821a28554133d0
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-07-05 15:36:08 +0200

    Remove examples/Messages.sh, no need for actual translations
    tr() still kept in code though, for people copying it

--- examples/Messages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Extract strings from all source files.
-# EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
-# lconvert.
-$EXTRACT_TR_STRINGS `find . -not -path ./plugins/\* \( -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml \) -type f` \
-    -o $podir/kreport_examples.pot
