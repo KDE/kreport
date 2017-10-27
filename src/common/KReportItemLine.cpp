KDE/kreport

commit 4e7e49cb2a105b1e56e751befb826edaa4c3fd8a

Remove KReportPosition and KReportSize.  Port the Line item to QPointF and KProperty.
Summary: Line does not currently display in the designer, but does display ok in the renderer.

Test Plan: Test items in kreportexample

Reviewers: staniek

Reviewed By: staniek

Tags: #kreport

Differential Revision: https://phabricator.kde.org/D3612

src/common/KReportItemLine.cpp
@@ -21,6 +21,7 @@
 
 #include <KPropertySet>
 
+#include <QCoreApplication>
 #include <QDomNode>
 
 KReportItemLine::KReportItemLine()
@@ -43,8 +44,8 @@ KReportItemLine::KReportItemLine(const QDomNode & element)
     _s.setY(KReportUnit::parseValue(element.toElement().attribute(QLatin1String("svg:y1"), QLatin1String("1cm"))));
     _e.setX(KReportUnit::parseValue(element.toElement().attribute(QLatin1String("svg:x2"), QLatin1String("1cm"))));
     _e.setY(KReportUnit::parseValue(element.toElement().attribute(QLatin1String("svg:y2"), QLatin1String("2cm"))));
-    m_start.setPointPos(_s);
-    m_end.setPointPos(_e);
+    m_start->setValue(_s);
+    m_end->setValue(_e);
 
     for (int i = 0; i < nl.count(); i++) {
         node = nl.item(i);
@@ -69,14 +70,19 @@ KReportItemLine::~KReportItemLine()
 
 void KReportItemLine::createProperties()
 {
-    m_lineWeight = new KProperty("line-weight", 1, tr("Line Weight"));
+    m_start = new KProperty("startposition", QPointF(), QCoreApplication::translate("StartPosition", "Start Position"));
+    m_end = new KProperty("endposition", QPointF(), QCoreApplication::translate("EndPosition", "End Position"));
+
+    m_lineWeight = new KProperty("line-weight", 1.0, tr("Line Weight"));
     m_lineColor = new KProperty("line-color", QColor(Qt::black), tr("Line Color"));
     m_lineStyle = new KProperty("line-style", (int)Qt::SolidLine, tr("Line Style"), tr("Line Style"), KProperty::LineStyle);
-    m_start.setName(QLatin1String("Start"));
-    m_end.setName(QLatin1String("End"));
-
-    propertySet()->addProperty(m_start.property());
-    propertySet()->addProperty(m_end.property());
+    
+    //Remove the unused properies from KReportItemBase
+    propertySet()->removeProperty("size");
+    propertySet()->removeProperty("position");
+    
+    propertySet()->addProperty(m_start);
+    propertySet()->addProperty(m_end);
     propertySet()->addProperty(m_lineWeight);
     propertySet()->addProperty(m_lineColor);
     propertySet()->addProperty(m_lineStyle);
@@ -113,8 +119,8 @@ int KReportItemLine::renderSimpleData(OROPage *page, OROSection *section, const 
     Q_UNUSED(data)
 
     OROLine * ln = new OROLine();
-    QPointF s = m_start.toScene();
-    QPointF e = m_end.toScene();
+    QPointF s = scenePosition(m_start->value().toPointF());
+    QPointF e = scenePosition(m_end->value().toPointF());
 
     s += offset;
     e += offset;
@@ -125,8 +131,8 @@ int KReportItemLine::renderSimpleData(OROPage *page, OROSection *section, const 
     if (page) page->insertPrimitive(ln);
 
     OROLine *l2 = dynamic_cast<OROLine*>(ln->clone());
-    l2->setStartPoint(m_start.toPoint());
-    l2->setEndPoint(m_end.toPoint());
+    l2->setStartPoint(m_start->value().toPointF());
+    l2->setEndPoint(m_end->value().toPointF());
     if (section) section->addPrimitive(l2);
 
     return 0;
@@ -134,17 +140,17 @@ int KReportItemLine::renderSimpleData(OROPage *page, OROSection *section, const 
 
 void KReportItemLine::setUnit(const KReportUnit &u)
 {
-    m_start.setUnit(u);
-    m_end.setUnit(u);
+    m_start->setOption("unit", u.symbol());
+    m_end->setOption("unit", u.symbol());
 }
 
-KReportPosition KReportItemLine::startPosition() const
+QPointF KReportItemLine::startPosition() const
 {
-    return m_start;
+    return m_start->value().toPointF();
 }
 
-KReportPosition KReportItemLine::endPosition() const
+QPointF KReportItemLine::endPosition() const
 {
-    return m_end;
+    return m_end->value().toPointF();
 }
 

autotests/format/FormatTest.cpp
@@ -27,8 +27,6 @@
 #include "KReportDocument.h"
 #include "KReportDesignerSectionDetail.h"
 #include "KReportSection.h"
-#include "KReportPosition.h"
-#include "KReportSize.h"
 #include "KReportDesignerItemLine.h"
 #include "KReportItemLine.h"
 #include "KReportDesignerItemRectBase.h"

src/CMakeLists.txt
@@ -9,8 +9,6 @@ endif()
 set(kreport_LIB_SRCS
     common/kreport_debug.cpp
     common/kreportplugin_debug.cpp
-    common/KReportPosition.cpp
-    common/KReportSize.cpp
     common/KReportItemBase.cpp
     common/KReportAsyncItemBase.cpp
     common/KReportSectionData.cpp
@@ -259,8 +257,6 @@ ecm_generate_headers(kreport_FORWARDING_HEADERS
         KReportUnit
         KReportUtils
         KReportDesign
-        KReportSize
-        KReportPosition
         KReportDocument
         KReportSectionData
         KReportRenderObjects

src/common/KReportItemBase.h
@@ -20,9 +20,8 @@
 
 #include "config-kreport.h"
 #include "kreport_export.h"
-#include "KReportPosition.h"
-#include "KReportSize.h"
 #include "KReportDpi.h"
+#include "KReportUnit.h"
 
 #include <QObject>
 #include <QFont>

src/common/KReportItemLine.h
@@ -19,7 +19,6 @@
 #define KREPORTITEMLINE_H
 
 #include "KReportItemBase.h"
-#include "KReportPosition.h"
 #include "kreport_export.h"
 
 class QDomNode;
@@ -44,12 +43,12 @@ public:
 
     virtual void setUnit(const KReportUnit&);
 
-    KReportPosition startPosition() const;
-    KReportPosition endPosition() const;
+    QPointF startPosition() const;
+    QPointF endPosition() const;
 
 protected:
-    KReportPosition m_start;
-    KReportPosition m_end;
+    KProperty *m_start;
+    KProperty *m_end;
     KProperty *m_lineColor;
     KProperty *m_lineWeight;
     KProperty *m_lineStyle;

src/common/KReportPosition.cpp
@@ -1,101 +1,0 @@
-/* This file is part of the KDE project
- * Copyright (C) 2007-2008 by Adam Pigg (adam@piggz.co.uk)
- * Copyright (C) 2010 Jarosław Staniek <staniek@kde.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "KReportPosition.h"
-#include "KReportDpi.h"
-
-#include <KProperty>
-
-#include <QApplication>
-
-KReportPosition::KReportPosition(const KReportUnit& unit)
-{
-    m_unit = unit;
-    m_property = new KProperty("position", toScene(), QCoreApplication::translate("KReportPosition", "Position"));
-}
-
-void KReportPosition::setName(const QString& name)
-{
-    m_property->setName(name.toLatin1());
-    m_property->setCaption(name);
-}
-
-KReportPosition::~KReportPosition()
-{
-}
-
-void KReportPosition::setScenePos(const QPointF& pos, UpdatePropertyFlag update)
-{
-    const qreal x = INCH_TO_POINT(pos.x() / KReportDpi::dpiX());
-    const qreal y = INCH_TO_POINT(pos.y() / KReportDpi::dpiY());
-
-    m_pointPos.setX(x);
-    m_pointPos.setY(y);
-
-    if (update == UpdateProperty)
-        m_property->setValue(toUnit());
-}
-
-void KReportPosition::setUnitPos(const QPointF& pos, UpdatePropertyFlag update)
-{
-    const qreal x = m_unit.fromUserValue(pos.x());
-    const qreal y = m_unit.fromUserValue(pos.y());
-
-    m_pointPos.setX(x);
-    m_pointPos.setY(y);
-
-    if (update == UpdateProperty)
-        m_property->setValue(toUnit());
-}
-
-void KReportPosition::setPointPos(const QPointF& pos, UpdatePropertyFlag update)
-{
-    m_pointPos.setX(pos.x());
-    m_pointPos.setY(pos.y());
-
-    if (update == UpdateProperty)
-        m_property->setValue(toUnit());
-}
-
-void KReportPosition::setUnit(const KReportUnit& u)
-{
-    m_unit = u;
-    m_property->setValue(toUnit());
-}
-
-QPointF KReportPosition::toPoint() const
-{
-    return m_pointPos;
-}
-
-QPointF KReportPosition::toScene() const
-{
-    const qreal x = POINT_TO_INCH(m_pointPos.x()) * KReportDpi::dpiX();
-    const qreal y = POINT_TO_INCH(m_pointPos.y()) * KReportDpi::dpiY();
-
-    return QPointF(x, y);
-}
-
-QPointF KReportPosition::toUnit() const
-{
-    const qreal x = m_unit.toUserValue(m_pointPos.x());
-    const qreal y = m_unit.toUserValue(m_pointPos.y());
-
-    return QPointF(x, y);
-}
-

src/common/KReportPosition.h
@@ -1,61 +1,0 @@
-/* This file is part of the KDE project
- * Copyright (C) 2007-2008 by Adam Pigg (adam@piggz.co.uk)
- * Copyright (C) 2010 Jarosław Staniek <staniek@kde.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#ifndef KRPOS_H
-#define KRPOS_H
-
-#include <QPointF>
-
-#include "kreport_export.h"
-#include "KReportUnit.h"
-
-class KProperty;
-
-/**
-*/
-class KREPORT_EXPORT KReportPosition
-{
-public:
-    enum UpdatePropertyFlag {
-        UpdateProperty,
-        DontUpdateProperty
-    };
-
-    explicit KReportPosition(const KReportUnit& unit = KReportUnit(KReportUnit::Centimeter));
-
-    ~KReportPosition();
-    QPointF toUnit() const;
-    QPointF toPoint() const;
-    QPointF toScene() const;
-    void setScenePos(const QPointF&, UpdatePropertyFlag update = UpdateProperty);
-    void setUnitPos(const QPointF&, UpdatePropertyFlag update = UpdateProperty);
-    void setPointPos(const QPointF&, UpdatePropertyFlag update = UpdateProperty);
-    void setUnit(const KReportUnit& unit);
-    void setName(const QString& name);
-    KReportUnit unit() const { return m_unit; }
-
-    KProperty* property() const {
-        return m_property;
-    }
-private:
-    QPointF m_pointPos;
-    KReportUnit m_unit;
-    KProperty* m_property;
-};
-
-#endif

src/common/KReportSize.cpp
@@ -1,96 +1,0 @@
-/* This file is part of the KDE project
- * Copyright (C) 2007-2008 by Adam Pigg (adam@piggz.co.uk)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "KReportSize.h"
-#include "KReportDpi.h"
-
-#include <KProperty>
-
-#include <QApplication>
-
-KReportSize::KReportSize(const KReportUnit& unit)
-{
-    m_unit = unit;
-    m_property = new KProperty("size", toScene(), QCoreApplication::translate("KReportSize", "Size"));
-}
-
-
-KReportSize::~KReportSize()
-{
-}
-
-void KReportSize::setSceneSize(const QSizeF& s, UpdatePropertyFlag update)
-{
-    qreal w, h;
-
-    w = INCH_TO_POINT(s.width() / KReportDpi::dpiX());
-    h = INCH_TO_POINT(s.height() / KReportDpi::dpiY());
-    m_pointSize.setWidth(w);
-    m_pointSize.setHeight(h);
-
-    if (update == UpdateProperty)
-        m_property->setValue(toUnit());
-}
-
-void KReportSize::setUnitSize(const QSizeF& s, UpdatePropertyFlag update)
-{
-    qreal w, h;
-    w = m_unit.fromUserValue(s.width());
-    h = m_unit.fromUserValue(s.height());
-    m_pointSize.setWidth(w);
-    m_pointSize.setHeight(h);
-
-    if (update == UpdateProperty)
-        m_property->setValue(toUnit());
-}
-
-void KReportSize::setPointSize(const QSizeF& s, UpdatePropertyFlag update)
-{
-    m_pointSize.setWidth(s.width());
-    m_pointSize.setHeight(s.height());
-
-    if (update == UpdateProperty)
-        m_property->setValue(toUnit());
-}
-
-void KReportSize::setUnit(KReportUnit u)
-{
-    m_unit = u;
-    m_property->setValue(toUnit());
-}
-
-QSizeF KReportSize::toPoint() const
-{
-    return m_pointSize;
-}
-
-QSizeF KReportSize::toScene() const
-{
-    qreal w, h;
-    w = POINT_TO_INCH(m_pointSize.width()) * KReportDpi::dpiX();
-    h = POINT_TO_INCH(m_pointSize.height()) * KReportDpi::dpiY();
-    return QSizeF(w, h);
-}
-
-QSizeF KReportSize::toUnit() const
-{
-    qreal w, h;
-    w = m_unit.toUserValue(m_pointSize.width());
-    h = m_unit.toUserValue(m_pointSize.height());
-
-    return QSizeF(w, h);
-}

commit 01c74dd4846346ce8092c93709a94dbaab5ed9e3

Fix crash when pasting line element
(unreleased regression from commit 4e7e49cb2a105, Dec 8 2016)

Solution is to avoid removing properties and hiding instead.

src/common/KReportItemLine.cpp
@@ -80,8 +80,8 @@ void KReportItemLine::createProperties()
                                 QString(), KProperty::LineStyle);
 
     //Remove the unused properies from KReportItemBase
-    propertySet()->removeProperty("size");
-    propertySet()->removeProperty("position");
+    propertySet()->property("size").setVisible(false);
+    propertySet()->property("position").setVisible(false);
 
     propertySet()->addProperty(m_start);
     propertySet()->addProperty(m_end);