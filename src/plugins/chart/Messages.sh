86ce6640fd783433af2ef467d5a73245afa6aba0
Jaroslaw Staniek <staniek@kde.org>
2017-11-17 15:38:06 +0100

    TRIVIAL The chart plugin is not in use so far: don't bother translators with its messages

--- src/plugins/chart/Messages.sh
+++ src/plugins/chart/Messages.sh
@@ -3,5 +3,8 @@
 # Extract strings from all source files.
 # EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
 # lconvert.
+
+exit 0 # TODO re-enable when this plugin is used
+
 $EXTRACT_TR_STRINGS `find . \( -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml \) -type f` \
     -o $podir/kreport_chartplugin_qt.pot
