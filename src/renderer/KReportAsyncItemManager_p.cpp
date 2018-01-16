df4177c0accf4c76368b830e3b5bf0f97fd77769
Adam Pigg <adam@piggz.co.uk>
2018-01-16 22:22:21 +0000

    Notify document when async item is rendered
    Summary: async items like web and map do not notify the document when an item is rendered
    
    Test Plan: Open report with web view and see renders appear as they become available
    
    Reviewers: staniek
    
    Tags: #kreport
    
    Differential Revision: https://phabricator.kde.org/D9933

--- src/renderer/KReportAsyncItemManager_p.cpp
+++ src/renderer/KReportAsyncItemManager_p.cpp
@@ -24,7 +24,7 @@
 #include <QPointF>
 
 namespace KReportPrivate {
-  
+
 class RenderData {
 public:
     KReportAsyncItemBase* item;
@@ -53,6 +53,7 @@ void AsyncItemManager::addItem(KReportAsyncItemBase* item, OROPage* page, OROSec
     rdata->section = section;
     rdata->offset = offset;
     rdata->data = data;
+
 #ifdef KREPORT_SCRIPTING
     rdata->script = script;
 #else
@@ -70,9 +71,12 @@ void AsyncItemManager::addItem(KReportAsyncItemBase* item, OROPage* page, OROSec
 
 void AsyncItemManager::itemFinished()
 {
+    m_curDoc->updated(m_curPage->pageNumber());
     //kreportDebug();
     if (m_renderList.count() > 0) {
         RenderData *rdata = m_renderList.dequeue();
+        m_curPage = rdata->page;
+        m_curDoc = m_curPage->document();
         rdata->item->renderSimpleData(rdata->page, rdata->section, rdata->offset, rdata->data, rdata->script);
     } else {
         emit(finished());
@@ -84,6 +88,8 @@ void AsyncItemManager::startRendering()
     //kreportDebug();
     if (m_renderList.count() > 0) {
         RenderData *rdata = m_renderList.dequeue();
+        m_curPage = rdata->page;
+        m_curDoc = m_curPage->document();
         rdata->item->renderSimpleData(rdata->page, rdata->section, rdata->offset, rdata->data, rdata->script);
     } else {
         emit(finished());
